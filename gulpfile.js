5fc4f8725a11316b0a399897a142214e1e97a7fb
AmirAli Akbari <amiraliakbari@gmail.com>
2017-04-08 17:10:18 +0430

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var replace = require('gulp-replace');
+var connect = require('gulp-connect');
+
+// Aliases
+gulp.task('default', ['serve']);
+gulp.task('build', ['copy', 'js']);
+
+
+// Copy
+gulp.task('copy', function() {
+    gulp.src(['*.html'])
+      .pipe(replace('src="/src/player.js"', 'src="/player.js"'))
+      .pipe(gulp.dest('dist'));
+    gulp.src(['7.9.1/**/*']).pipe(gulp.dest('dist/7.9.1'));
+});
+
+
+// JS
+gulp.task('js', function (cb) {
+  pump([
+        gulp.src('src/**/*.js'),
+        uglify(),
+        gulp.dest('dist')
+    ],
+    cb
+  );
+});
+
+// Serve
+gulp.task('serve', function() {
+  connect.server({
+    port: 8080
+  });
+});
+
