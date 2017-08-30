56f41e84f92875c96f79aa3326517891b0f54f30
AmirAli Akbari <amiraliakbari@gmail.com>
2017-08-30 13:45:18 +0430

    updated build step accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var uglify = require('gulp-uglify');
 var pump = require('pump');
 var replace = require('gulp-replace');
 var connect = require('gulp-connect');
+var clean = require('gulp-clean');
 
 // Aliases
 gulp.task('default', ['serve']);
@@ -10,20 +11,21 @@ gulp.task('build', ['copy', 'js']);
 
 
 // Copy
-gulp.task('copy', function() {
+gulp.task('copy', ['clean'], function() {
     gulp.src(['*.html'])
       .pipe(replace('src="/src/player.js"', 'src="/player.js"'))
       .pipe(gulp.dest('dist'));
+    gulp.src(['fonts/**/*', 'content/**/*', 'icons/**/*', 'vpaid/**/*', 'check/**/*'], {base: '.'}).pipe(gulp.dest('dist'));
     gulp.src(['*.ico', '*.gif', '*.jpg', '*.png']).pipe(gulp.dest('dist'));
     gulp.src(['_headers', 'crossdomain.xml', 'vpaid.xml']).pipe(gulp.dest('dist'));
-    gulp.src(['7.9.1/**/*']).pipe(gulp.dest('dist/7.9.1'));
-    gulp.src(['7.10.4/**/*', '!7.10.4/**/*.js']).pipe(gulp.dest('dist/7.10.4'));
-    gulp.src(['7.12.1/**/*', '!7.12.1/**/*.js']).pipe(gulp.dest('dist/7.12.1'));
+    gulp.src(['bundle/7.9.1/**/*']).pipe(gulp.dest('dist/7.9.1'));
+    gulp.src(['bundle/7.10.4/**/*', '!bundle/7.10.4/**/*.js']).pipe(gulp.dest('dist/7.10.4'));
+    gulp.src(['bundle/7.12.1/**/*', '!bundle/7.12.1/**/*.js']).pipe(gulp.dest('dist/7.12.1'));
 });
 
 
 // JS
-gulp.task('js', function (cb) {
+gulp.task('js', ['clean', 'copy'], function (cb) {
   pump([
         gulp.src(['src/**/*.js', '**/*.js', '!node_modules/**/*', '!dist/**/*.js']),
         uglify(),
@@ -31,6 +33,12 @@ gulp.task('js', function (cb) {
     ],
     cb
   );
+});
+
+// Clean
+gulp.task('clean', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
 });
 
 // Serve
